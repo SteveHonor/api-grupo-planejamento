2a0c6e51b6e48fbdc7030bd45f20765920e78ccd
Steve J <steve@honors.com.br>
2018-06-11 21:12:57 -0300

    cors for all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://grupoplanejamento.com.br'
+    origins '*'
 
     resource '*',
       headers: :any,
